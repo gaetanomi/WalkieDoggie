d19f8c7feb79a3ab89efdfdec800d5dfb0ba7bda
gaetanomi <113607731+gaetanomi@users.noreply.github.com>
2022-12-07 15:39:11 +0100

    init
    init

---
+++ WalkieDoggie/WalkieDoggie/WalkieDoggieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkieDoggieApp.swift
+//  WalkieDoggie
+//
+//  Created by GaetanoMiranda on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WalkieDoggieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
